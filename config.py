52e8d7c4324ad289e81a1f97223a243dc70e1881
sarthak7509 <60735499+sarthak7509@users.noreply.github.com>
2020-10-22 21:42:44 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+BASE_PATH = "dataset"
+IMAGES_PATH = os.path.sep.join([BASE_PATH,"images"])
+ANNOTS_PATH = os.path.sep.join([BASE_PATH,"annotations"])
+BASE_OUTPUT = "output"
+MODEL_PATH = os.path.sep.join([BASE_OUTPUT, "dog_detector.h5"])
+LB_PATH = os.path.sep.join([BASE_OUTPUT, "lb.pickle"])
+PLOTS_PATH = os.path.sep.join([BASE_OUTPUT, "plots"])
+TEST_PATHS = os.path.sep.join([BASE_OUTPUT, "test_paths.txt"])
+
+INIT_LR = 1e-4
+NUM_EPOCHS = 20
+BATCH_SIZE = 32
---
+++ train.py
@@ -0,0 +1,163 @@
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import config
+import cv2
+import os
+
+print("[+]loading datasets...")
+data=[]
+labels=[]
+bboxes =[]
+imagePaths=[]
+jpg = ".jpg"
+for csvPath in paths.list_files(config.ANNOTS_PATH, validExts=(".csv")):
+	rows = open(csvPath).read().strip().split("\n")
+	for row in rows:
+		row = row.split(",")
+		(filename, startX, startY, endX, endY, label) = row
+		imagePath = os.path.sep.join([config.IMAGES_PATH, label,
+			filename])
+		image = cv2.imread(imagePath+jpg)
+		(h, w) = image.shape[:2]
+		startX = float(startX) / w
+		startY = float(startY) / h
+		endX = float(endX) / w
+		endY = float(endY) / h
+		image = load_img(imagePath+jpg, target_size=(224, 224))
+		image = img_to_array(image)
+		data.append(image)
+		labels.append(label)
+		bboxes.append((startX, startY, endX, endY))
+		imagePaths.append(imagePath+jpg)
+
+data = np.array(data, dtype="float32") / 255.0
+labels = np.array(labels)
+bboxes = np.array(bboxes, dtype="float32")
+imagePaths = np.array(imagePaths)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+if len(lb.classes_) == 2:
+	labels = to_categorical(labels)
+
+split = train_test_split(data, labels, bboxes, imagePaths,
+	test_size=0.20, random_state=42)
+
+(trainImages, testImages) = split[:2]
+(trainLabels, testLabels) = split[2:4]
+(trainBBoxes, testBBoxes) = split[4:6]
+(trainPaths, testPaths) = split[6:]
+
+print("[INFO] saving testing image paths...")
+f = open(config.TEST_PATHS, "w")
+f.write("\n".join(testPaths))
+f.close()
+
+vgg = VGG16(weights="imagenet", include_top=False,
+	input_tensor=Input(shape=(224, 224, 3)))
+vgg.trainable = False
+flatten = vgg.output
+flatten = Flatten()(flatten)
+
+bboxHead = Dense(128, activation="relu")(flatten)
+bboxHead = Dense(64, activation="relu")(bboxHead)
+bboxHead = Dense(32, activation="relu")(bboxHead)
+bboxHead = Dense(4, activation="sigmoid",
+	name="bounding_box")(bboxHead)
+
+softmaxHead = Dense(512, activation="relu")(flatten)
+softmaxHead = Dropout(0.5)(softmaxHead)
+softmaxHead = Dense(512, activation="relu")(softmaxHead)
+softmaxHead = Dropout(0.5)(softmaxHead)
+softmaxHead = Dense(len(lb.classes_), activation="softmax",
+	name="class_label")(softmaxHead)
+
+model = Model(
+	inputs=vgg.input,
+	outputs=(bboxHead, softmaxHead))
+
+losses = {
+	"class_label": "categorical_crossentropy",
+	"bounding_box": "mean_squared_error",
+}
+
+lossWeights = {
+	"class_label": 1.0,
+	"bounding_box": 1.0
+}
+
+opt = Adam(lr=config.INIT_LR)
+model.compile(loss=losses, optimizer=opt, metrics=["accuracy"], loss_weights=lossWeights)
+print(model.summary())
+
+trainTargets = {
+	"class_label": trainLabels,
+	"bounding_box": trainBBoxes
+}
+testTargets = {
+	"class_label": testLabels,
+	"bounding_box": testBBoxes
+}
+
+print("[INFO] training model...")
+H = model.fit(
+	trainImages, trainTargets,
+	validation_data=(testImages, testTargets),
+	batch_size=config.BATCH_SIZE,
+	epochs=config.NUM_EPOCHS,
+	verbose=1)
+
+print("[INFO] saving object detector model...")
+model.save(config.MODEL_PATH, save_format="h5")
+
+print("[INFO] saving label binarizer...")
+f = open(config.LB_PATH, "wb")
+f.write(pickle.dumps(lb))
+f.close()
+
+lossNames = ["loss", "class_label_loss", "bounding_box_loss"]
+N = np.arange(0, config.NUM_EPOCHS)
+plt.style.use("ggplot")
+(fig, ax) = plt.subplots(3, 1, figsize=(13, 13))
+
+for (i, l) in enumerate(lossNames):
+	title = "Loss for {}".format(l) if l != "loss" else "Total loss"
+	ax[i].set_title(title)
+	ax[i].set_xlabel("Epoch #")
+	ax[i].set_ylabel("Loss")
+	ax[i].plot(N, H.history[l], label=l)
+	ax[i].plot(N, H.history["val_" + l], label="val_" + l)
+	ax[i].legend()
+
+plt.tight_layout()
+plotPath = os.path.sep.join([config.PLOTS_PATH, "losses.png"])
+plt.savefig(plotPath)
+plt.close()
+
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(N, H.history["class_label_accuracy"],
+	label="class_label_train_acc")
+plt.plot(N, H.history["val_class_label_accuracy"],
+	label="val_class_label_acc")
+plt.title("Class Label Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Accuracy")
+plt.legend(loc="lower left")
+
+plotPath = os.path.sep.join([config.PLOTS_PATH, "accs.png"])
+plt.savefig(plotPath)
